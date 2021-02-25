6df2042bd1f00ad07327b352ea08bfaba249d0e6
Jonathan Hill <76394328+Jonathan-R-Hill@users.noreply.github.com>
2021-02-25 14:18:08 +0000

    Completes hurdle 4

--- Projects/reeborg.py
+++ Projects/reeborg.py
@@ -1,26 +1,31 @@
 """
-Hurdle 3 at: 
+Hurdle 4 at: 
 https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%203&url=worlds%2Ftutorial_en%2Fhurdle3.json
 
 """
-
 def turn_right():
     turn_left()
     turn_left()
     turn_left()
     
 def jump():
-    turn_left()
-    move()
     turn_right()
     move()
     turn_right()
     move()
-    turn_left()
     
 while not at_goal():
-    
-    if wall_in_front():
+    if not wall_on_right and wall_in_front():
         jump()
+    
+    elif not wall_on_right and is_facing_north():
+        jump()
+    
+    elif wall_in_front() and wall_on_right():
+        turn_left()
+        
+    elif wall_on_right():
+        move()
     else:
+        turn_right()
         move()
