ce87450dd62cfc737cd758072a44be24c2d5f26b
Jonathan Hill <76394328+Jonathan-R-Hill@users.noreply.github.com>
2021-02-26 16:49:50 +0000

    added logo, Used % instead of making letters *2

--- Projects/Days 1 - 10/Caesars Cipher.py
+++ Projects/Days 1 - 10/Caesars Cipher.py
@@ -1,8 +1,25 @@
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+     """
+
 valid = ['encode', 'decode']
 valid1 = ['yes', 'no']
 
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-           'n', 'o','p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']*10
+           'n', 'o','p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 def encode(message, shift):
     encrypted_message = ''
@@ -13,7 +30,7 @@ def encode(message, shift):
               
         elif letter in letters:
             x = letters.index(letter)
-            letter = letters[x + shift]
+            letter = letters[(x + shift) % 26]
             encrypted_message += letter
         
             
@@ -29,13 +46,14 @@ def decode(message, shift):
             
         elif letter in letters:
             x = letters.index(letter)
-            letter = letters[x - shift + 26]
+            letter = letters[(x - shift) % 26]
             decrypted_message += letter
             
     print(f"here is your decoded message\n{decrypted_message}")
 
 # Loops through the game untill carry_on is False
 carry_on = True
+print(logo)
 while carry_on == True:
 # Checks for valid input (encode / decode) if not valid it will ask again
     while True:
